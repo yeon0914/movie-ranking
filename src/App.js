e5ad49ce707d0bf534d6158795dc8c0766e92dfc
chaeyeon-kimm <chaeyeon.kimm@navercorp.com>
2022-12-18 16:46:27 +0900

    init : react, express 연동

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    fetch("api")
+      .then((res) => res.json())
+      .then((data) => setUsername(data.username));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {username ? `Hello ${username}` : "Hello World"}
       </header>
     </div>
   );
